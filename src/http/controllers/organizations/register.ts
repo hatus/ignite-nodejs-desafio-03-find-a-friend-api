cdac5c1991c6c69529f59e804127e9a35dc50b9a
Hatus Níwman <hatusn@gmail.com>
2023-06-13 10:56:50 -0300

    fix path files

--- src/http/controllers/organizations/register.ts
+++ src/http/controllers/organizations/register.ts
@@ -1,6 +1,6 @@
+import { OrganizationAlreadyExistsError } from '@/use-cases/errors/organization-already-exists-error'
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { makeRegisterUseCase } from '../../use-cases/factories/make-register-use-case'
-import { OrganizationAlreadyExistsError } from '../../use-cases/errors/organization-already-exists-error'
 import { z } from 'zod'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
